748e2490fef9bab80db943820e147bd533d9d03e
WisdomOfogba <sannishazily@gmail.com>
2025-10-06 22:18:51 +0100

    fix: Correct CSS import in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import AppRouter from "./AppRouter";
-import "./app.css";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
